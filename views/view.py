17eec809bb60658ea290fac3dc8d84a577598254
Edgar Casimiro <edgarcasimiro@MacBook-Air-de-Edgar.local>
2022-04-26 19:48:05 +0100

    NovoFinal

--- views/view.py
+++ views/view.py
@@ -10,16 +10,21 @@ def main():
             return
         if comandos[0] == "RPI": #Registar inicio da lista
             lista_ligada.insert_at_start(comandos[1])
+            lista_ligada.traverse_list()
         elif comandos[0] == "RPF": #Registar no fim da lista
             lista_ligada.insert_at_end(comandos[1])
+            lista_ligada.traverse_list()
         elif comandos[0] == "RPDE": #Registar depois de outro elemento
             lista_ligada.insert_after_item(comandos[2],comandos[1])
+            lista_ligada.traverse_list()
         elif comandos[0] == "RPAE": #Registar antes de outro elemento
             lista_ligada.insert_before_item(comandos[2],comandos[1])
+            lista_ligada.traverse_list()
         elif comandos[0] == "RPII": #Registar num determinado indice
             lista_ligada.insert_at_index(int(comandos[2]),comandos[1])
+            lista_ligada.traverse_list()
         elif comandos[0] == "VNE": #Verificar numero de elementos
-            print("O número de elementos são " + str(lista_ligada.get_count()))
+            print("O número de elementos são " + str(lista_ligada.get_count())+".")
         elif comandos[0] == "VP": #Verificar se pais se encontra na lista
             if lista_ligada.search_item(comandos[1]) == False:
                 print("O país "+str(comandos[1])+" não se encontra na lista")
@@ -40,6 +45,4 @@ def main():
         else:
             if comandos[0] == "":
                 break
-        #print lista
-        lista_ligada.traverse_list()
     
